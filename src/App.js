91b06869f92af6769dbf886ce8c7204d0d01dfc8
Muhammad Naveed <mnaveed47012@gmail.com>
2023-01-02 08:42:06 +0500

    Load dummy data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import ExpenseItem from './components/ExpenseItem';
+import expences from './data/expenses'
 
 import './App.css';
 
 function App() {
-  const expences = [
-    {id: 'e1', title: 'Expense Item 1', amount: 333.2, date: new Date(2001, 1, 1) },
-    {id: 'e2', title: 'Expense Item 2', amount: 33.2, date: new Date(2004, 1, 1) },
-    {id: 'e3', title: 'Expense Item 3', amount: 13.2, date: new Date(2020, 1, 1) },
-    {id: 'e4', title: 'Expense Item 4', amount: 48.2, date: new Date(2022, 1, 1) }
-  ]
   return (
     <div className="App">
       <h1>Let's get started</h1>
